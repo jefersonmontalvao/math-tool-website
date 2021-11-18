f825c3ec825daf8917d62ccb06c35e76ae27ec88
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-17 21:53:31 -0300

    Update loader.js

--- mathtool/js/scripts/loader.js
+++ mathtool/js/scripts/loader.js
@@ -1,6 +1,6 @@
 let elements = [
     {
-        path: "/mathtool/js/modules/math/main.js",
+        src: "\\mathtool\\js\\modules\\math\\main.js",
         type: "module",
     },
 ];
@@ -9,7 +9,7 @@ let elements = [
 function loadScripts(elements) {
     for (item of elements) {
         const elementHTML = document.createElement("script");
-        elementHTML.setAttribute("src", item.path);
+        elementHTML.setAttribute("src", item.src);
         elementHTML.setAttribute("type", item.type);
         document.body.appendChild(elementHTML);
     }
