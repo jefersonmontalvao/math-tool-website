f5a6b31d1f0fb7f888c93977acbb6bc3c0a5ec4c
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-20 01:26:04 -0300

    Create main.js

---
+++ mathtool/js/modules/content/main.js
@@ -0,0 +1,6 @@
+import { navHandler } from "./nav.js";
+import { initialRender } from "./utils.js"
+
+// Events
+window.addEventListener("load", navHandler);
+window.addEventListener("load", initialRender);
