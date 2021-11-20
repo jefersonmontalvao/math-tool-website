92afccc044fa65015ad00c14b4987d94d61226ed
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-20 01:26:18 -0300

    Create templates.js

---
+++ mathtool/js/modules/content/templates.js
@@ -0,0 +1,13 @@
+let divisionTemplate =
+    '    <div class="division-input-block">' +
+    '<label for="division-form" class="division-input-block__label">Insira os números abaixo:</label>' +
+    '<form id="division-form" class="division-form division-input-block__division-form">' +
+    '    <label for="input-dividend" class="division-form__label">Dividendo:</label>' +
+    '    <input type="text" id="input-dividend" placeholder="Número Dividendo..." class="division-form__input division-form__dividend">' +
+    '    <label for="input-divider" class="division-form__label">Divisor:</label>' +
+    '    <input type="text" id="input-divider" placeholder="Número Divisor..." class="division-form__input division-form__divider">' +
+    '    <input type="button" value="Calcular" class="division-form__input division-form__btn-calculate">' +
+    "</form>" +
+    "</div>";
+
+export { divisionTemplate };
