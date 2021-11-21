566431eb3221673bbad61d5f5b2f356ead1c2498
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-21 16:17:02 -0300

    Delete utils.js

--- mathtool/js/modules/content/utils.js
+++
@@ -1,9 +0,0 @@
-import { homeRender } from './renders.js'
-
-function initialRender() {
-    let logoElement = document.querySelector(".site-header__title");
-    logoElement.addEventListener("click", homeRender);
-    homeRender();
-}
-
-export { initialRender };
