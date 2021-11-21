a5231113d332fdcdc3aaeaf4395cb7a1203539f7
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-21 16:15:03 -0300

    Update nav.js

--- mathtool/js/modules/content/nav.js
+++ mathtool/js/modules/content/nav.js
@@ -1,28 +1,36 @@
-import { homeRender, divisionRender } from "./renders.js"
+import { homeRender, divisionRender } from "./renders.js";
 
-let navData = [
+let navLoadData = [
     {
         label: "Início",
         render: homeRender,
     },
     {
         label: "Divisão",
-        render: divisionRender
-    }
+        render: divisionRender,
+    },
 ];
 
-function navHandler() {
+function initNavHandler() {
     const navElement = document.querySelector(".site-nav");
 
     // Set to load home page.
-    for (let data of navData) {
+    for (let data of navLoadData) {
         const navLabelElement = document.createElement("a");
         navLabelElement.setAttribute("class", "site-nav__anchor");
         navLabelElement.innerHTML = data.label;
         navLabelElement.addEventListener("click", data.render);
-        
+
         navElement.appendChild(navLabelElement);
     }
+
+    // Initialize the first view.
+    // Add event on site logo.
+    let logoElement = document.querySelector(".site-header__title");
+    logoElement.addEventListener("click", homeRender);
+
+    // Render the Home page.
+    homeRender();
 }
 
-export { navHandler };
+export { initNavHandler };
