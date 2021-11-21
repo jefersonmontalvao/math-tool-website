acedbecb6c53b311b4a62766628fd345b1834ddb
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-21 16:16:57 -0300

    Update renders.js

--- mathtool/js/modules/content/renders.js
+++ mathtool/js/modules/content/renders.js
@@ -1,13 +1,15 @@
 import { divisionTemplate } from "./templates.js";
+import { addVerifyDivisionInputsEvent } from "./content_verification.js";
 
 var pageView = document.querySelector(".page-view");
 
 function homeRender() {
-    pageView.innerHTML = "<h1>Hello World!</h1>"
+    pageView.innerHTML = "<h1>Hello World!</h1>";
 }
 
 function divisionRender() {
     pageView.innerHTML = divisionTemplate;
+    addVerifyDivisionInputsEvent();
 }
 
 export { homeRender, divisionRender };
