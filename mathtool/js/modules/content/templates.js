1312c333878c799a1a593ba3e458738e02f4b7b1
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-24 18:23:19 -0300

    Update templates.js

--- mathtool/js/modules/content/templates.js
+++ mathtool/js/modules/content/templates.js
@@ -1,16 +1,30 @@
-let divisionTemplate =
-    `<div class="division-input-block">
-        <label for="division-form" class="division-input-block__label">Insira os números abaixo:</label>
+const homeTemplate = 
+`
+<h1>Hello World</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam sapiente temporibus consequuntur incidunt delectus in. Et facere eius quos delectus tempora? Adipisci nemo asperiores nisi minima praesentium ut iusto. Molestiae!</p>
+`;
 
-        <form id="division-form" class="division-form division-input-block__division-form" autocomplete="off">
-            <label for="input-dividend" class="division-form__label">Dividendo:</label>
-            <input type="number" id="input-dividend" placeholder="Número Dividendo..." class="division-form__input division-form__dividend">
+const divisionTemplate =
+    `
+    <div class="container site-main__container">
+    <h1 class="site-main__headline">Quer calcular uma divisão?</h1>
 
-            <label for="input-divider" class="division-form__label">Divisor:</label>
-            <input type="number" id="input-divider" placeholder="Número Divisor..." class="division-form__input division-form__divider">
+    <form autocomplete="off" class="division-form">
+        <div class="container form-division__input-container">
+            <label for="dividend" class="division-form__label">Dividendo</label>
+            <input type="number" name="dividend" placeholder="Qual número quer dividir?" id="dividend" class="division-form__dividend-input">
+        </div>
 
-            <input type="button" value="Calcular" class="division-form__input division-form__btn-calculate">
-        </form>
-    </div>`;
+        <div class="container form-division__input-container">
+            <label for="divider" class="division-form__label">Divisor</label>
+            <input type="number" name="divider" placeholder="Dividir por quanto?" id="divider" class="division-form__divider-input">
+        </div>
 
-export { divisionTemplate };
+        <div class="container form-division__input-container">
+            <input type="button" value="Calcular" class="division-form__btn--blue">
+        </div>
+    </form>
+    </div>
+`;
+
+export { divisionTemplate, homeTemplate };
