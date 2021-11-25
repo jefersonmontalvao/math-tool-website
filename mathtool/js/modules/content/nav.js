e1cd6f5dfe4d7c29cc796cfbe731dfab936563f3
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-24 23:10:56 -0300

    Update nav.js

--- mathtool/js/modules/content/nav.js
+++ mathtool/js/modules/content/nav.js
@@ -1,10 +1,6 @@
-import { homeRender, divisionRender } from "./renders.js";
+import { divisionRender } from "./renders.js";
 
 let navMetaData = [
-    {
-        label: "Home",
-        render: homeRender,
-    },
     {
         label: "Division",
         render: divisionRender,
@@ -21,12 +17,6 @@ function initNavHandler() {
             }
         });
     }
-
-    // Initialize the first view.
-    // Add event on site logo.
-    let logoElement = document.querySelector(".site-header__title");
-    logoElement.addEventListener("click", homeRender);
-    homeRender();
 }
 
 export { initNavHandler };
